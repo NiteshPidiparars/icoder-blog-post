3b1a42be5755025249f83e65e4771b278b658d73
Nitesh Pidiparas <niteshpidiparas76@gmail.com>
2021-06-06 12:11:14 +0530

    second commit

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hb9gi1i)qbpcx0*3*9s9ou5v8y4-sgp7eg$*l(xtc1jwk+m^*7'
+# SECRET_KEY = 'django-insecure-hb9gi1i)qbpcx0*3*9s9ou5v8y4-sgp7eg$*l(xtc1jwk+m^*7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["icoder-blog-post.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
