19c5a333faf520b4133a0fa5d5ccf37320ed4181
Nitesh Pidiparas <niteshpidiparas76@gmail.com>
2021-06-16 12:35:48 +0530

    one more commit

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
 ]
@@ -132,6 +134,11 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 #  Add configuration for static files storage using whitenoise
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- iCoder/urls.py
+++ iCoder/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
-admin.site.site_header="iCoder Admin"
-admin.site.site_title="iCoder Admin Panel"
-admin.site.index_title="Welcome to iCoder Admin Panel"
+admin.site.site_header = "iCoder Admin"
+admin.site.site_title = "iCoder Admin Panel"
+admin.site.index_title = "Welcome to iCoder Admin Panel"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
